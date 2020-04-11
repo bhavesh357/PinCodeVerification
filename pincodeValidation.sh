321637ed2c5a1b6baf9cc833f0472f98387b92c9
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-11 15:27:39 +0530

    ADDED: function to validate pincode

--- pincodeValidation.sh
+++ pincodeValidation.sh
@@ -1,1 +1,16 @@
 echo "Welcome to pin code verification"
+
+function validatePincode() {
+	pincode=$1
+	pat="\d{6}"
+	if [[ $pincode =~ $pat ]]
+	then
+		echo good pincode
+	else
+		echo enter proper pincode
+	fi
+}
+
+echo "Enter pincode"
+read pincode
+validatePincode $pincode
